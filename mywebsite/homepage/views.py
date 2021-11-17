3c180eed2de39b96cb3e4225b565a72270eefe4c
corneliu12 <42445417+corneliu12@users.noreply.github.com>
2021-11-16 20:37:36 -0600

    added bootstrap template

--- mywebsite/homepage/views.py
+++ mywebsite/homepage/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello. This is the home page! Welcome")
+    return render(request, 'index.html')
