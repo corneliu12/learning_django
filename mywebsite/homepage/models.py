13523789715b210f61a85b698b96632584464186
corneliu12 <42445417+corneliu12@users.noreply.github.com>
2021-11-17 21:23:32 -0600

    create superuser and models

--- mywebsite/homepage/models.py
+++ mywebsite/homepage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class news(models.Model):
+    title = models.CharField(max_length=50, blank=False)
+    content = models.TextField(max_length=800, blank=False)
+    image = models.ImageField(upload_to='news/', blank=False)
+
+    def __str__(self):
+        return self.title
